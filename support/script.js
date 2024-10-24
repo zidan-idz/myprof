f4f7af60a58ea8f658f31b48f20a82123844872b
Muhammad Raid Zaidani <60657412+zidan-idz@users.noreply.github.com>
2024-10-25 00:27:00 +0800

    Update script.js

--- support/script.js
+++ support/script.js
@@ -1,6 +1,6 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function() {
-    const whiteBox = document.querySelector(".white-box");
+    const whiteBox = document.querySelector(".container");
 
     whiteBox.style.transform = "translateX(100%)";
     whiteBox.style.transition = "transform 1s ease";
